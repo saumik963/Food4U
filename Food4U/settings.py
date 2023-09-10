c2437ab22e51831cf1d1a0973b82a07591eeaecb
saumik963 <119744035+saumik963@users.noreply.github.com>
2023-09-10 21:37:22 +0600

    Update settings.py

--- Food4U/settings.py
+++ Food4U/settings.py
@@ -60,7 +60,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
